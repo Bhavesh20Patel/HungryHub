2ac47570fa13d65ad7d0aa46268002532fe0320e
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 21:51:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,24 @@ const app = express()
 const port = 5000
 const mongoDB = require("./db")
 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-requested-With, Content-Type, Accept"
-  );
-  next();
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-requested-With, Content-Type, Accept"
+//   );
+//   next();
 
-})
+// })
+
+app.use(cors (
+{
+origin: ["https://deploy-men-1whq.vercel.app"],
+methods: ["POST", "GET"],
+credentials: true
+}
+));
+
 mongoDB() ;
 app.get('/', (req, res) => {
   res.send('Hello World!')
