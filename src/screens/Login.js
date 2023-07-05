88457aa8860ddb2538e0509a60d253ed7c28c665
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 23:09:29 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,29 +1,30 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
 
-  const [credentials, setcredentials] = useState({email:"", password:""})
+  const [credentials, setcredentials] = useState({ email: "", password: "" })
   let navigate = useNavigate()
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser",{
-      method:'POST',
-      headers:{
-        'Content-Type' : 'application/json'
+    const response = await fetch("http://localhost:5000/api/loginuser", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify({password:credentials.password, email:credentials.email})
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
     });
     const json = await response.json()
     console.log(json)
 
-    if(!json.success){
+    if (!json.success) {
       alert("Enter Valid Credentials")
     }
 
-    if(json.success){
-      localStorage.setItem('userEmail', credentials.email)
-      localStorage.setItem("authToken",json.authToken)
+    if (json.success) {
+      localStorage.setItem("userEmail", credentials.email)
+      localStorage.setItem("authToken", json.authToken)
+      console.log(localStorage.getItem("authToken"))
       navigate("/");
     }
     else {
@@ -31,31 +32,32 @@ export default function Login() {
     }
   }
 
-  const onChange = (event)=>{
-    setcredentials({...credentials,[event.target.name]:event.target.value})
+  const onChange = (event) => {
+    setcredentials({ ...credentials, [event.target.name]: event.target.value })
   }
   return (
     <div>
 
-<div className="container">
-      <form onSubmit={handleSubmit}>
-        
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+      <div className="container" style={{marginTop:"50px"}} >
+       
+          <form onSubmit={handleSubmit}>
+
+            <div className="mb-3">
+              {/* <header>Login</header> */}
+              <label htmlFor="exampleInputEmail1" className="form-label" style={{color:"black"}}>Email address</label>
+              <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
+            </div>
+
+            <button type="submit" className="btn m-3 btn-success">Login</button>
+            <Link to="/signup" className="m-3 btn btn-danger sign">I am a new User</Link>
+          </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" name='password' value={credentials.password} onChange={onChange} id="exampleInputPassword1" />
-        </div>
-        
-        <button type="submit" className="btn m-3 btn-success">Login</button>
-        <Link to="/signup" className="m-3 btn btn-danger">I am a new User</Link>
-      </form>
+
       </div>
-
-
-    </div>
+   
   )
 }
