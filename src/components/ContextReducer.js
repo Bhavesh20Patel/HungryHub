97f023b823618cb600355618c64b625698a2d29e
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 23:05:54 +0530

    Update ContextReducer.js

--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useReducer } from 'react'
 
+
 const CartStateContext = createContext();
 const CartDispatchContext = createContext();
 
@@ -7,23 +8,26 @@ const reducer = (state, action) => {
     switch (action.type) {
         case "ADD":
             return [...state, { id: action.id, name: action.name, qty: action.qty, size: action.size, price: action.price, img: action.img }]
+
         case "REMOVE":
             let newArr = [...state]
             newArr.splice(action.index, 1)
             return newArr;
-        case "DROP":
-            let empArray = []
-            return empArray
+
         case "UPDATE":
             let arr = [...state]
             arr.find((food, index) => {
                 if (food.id === action.id) {
-                    console.log(food.qty, parseInt(action.qty), action.price + food.price)
-                    arr[index] = { ...food, qty: parseInt(action.qty) + food.qty, price: action.price + food.price }
+                    console.log(food.qty, parseFloat(action.qty), action.price + food.price)
+                    arr[index] = { ...food, qty: parseFloat(action.qty) + food.qty, price: action.price + food.price }
                 }
                 return arr
             })
             return arr
+
+        case "DROP":
+            let empArray = []
+            return empArray
         default:
             console.log("Error in Reducer");
     }
@@ -33,11 +37,12 @@ export const CartProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(reducer, [])
     return (
-        <CartDispatchContext.Provider value={dispatch}>
-            <CartDispatchContext.Provider value={state}>
+
+        <CartStateContext.Provider value={state}>
+            <CartDispatchContext.Provider value={dispatch}>
                 {children}
             </CartDispatchContext.Provider>
-        </CartDispatchContext.Provider>
+        </CartStateContext.Provider>
     )
 }
 
