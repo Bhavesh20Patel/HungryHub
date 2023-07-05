34f1f8351a40bd3aa707cee9df96c1d949b61b75
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 23:10:00 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -4,11 +4,11 @@ import Navbar from '../components/Navbar';
 
 export default function MyOrder() {
 
-    const [orderData, setorderData] = useState({})
+    const [orderData, setorderData] = useState("");
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/auth/myOrderData", {
+        await fetch("http://localhost:5000/api/myOrderData", {
           
             method: 'POST',
             headers: {
@@ -21,8 +21,6 @@ export default function MyOrder() {
             let response = await res.json()
             await setorderData(response)
         })
-
-
 
         // await res.map((data)=>{
         //    console.log(data)
@@ -60,13 +58,13 @@ export default function MyOrder() {
 
                                                         <div className='col-12 col-md-6 col-lg-3' >
                                                             <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                                                                <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} />
+                                                                {/* <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} /> */}
                                                                 <div className="card-body">
                                                                     <h5 className="card-title">{arrayData.name}</h5>
                                                                     <div className='container w-100 p-0' style={{ height: "38px" }}>
                                                                         <span className='m-1'>{arrayData.qty}</span>
                                                                         <span className='m-1'>{arrayData.size}</span>
-                                                                        <span className='m-1'>{data}</span>
+                                                                        {/* <span className='m-1'>{data}</span> */}
                                                                         <div className=' d-inline ms-2 h-100 w-20 fs-5' >
                                                                             ₹{arrayData.price}/-
                                                                         </div>
