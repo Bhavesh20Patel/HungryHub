7b4f0d65c1bbd448969f78a9cf1bd64d3be1ca5b
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 23:05:21 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,25 +8,49 @@ export default function Card(props) {
     let options = props.options;
     let priceOptions = Object.keys(options);
     const [qty, setQty] = useState(1)     //have to pass this as a data for the user 
-    const [size, setSize] = useState("")     
+    const [size, setSize] = useState("")  
+    
     const handleAddToCart = async ()=>{
+        let food = []
+        for(const item of data){
+            if(item.id === props.foodItem._id){
+                food = item;
+                
+                break;
+            }
+        }
+
+        if(food !== []){
+            if(food.size === size){
+                await dispatch({type:"UPDATE", id:props.foodItem._id, price: finalPrice, qty:qty})
+                return
+            }
+            else if(food.size !== size){
+                await dispatch({type:"ADD",id:props.foodItem._id,name:props.foodItem.name, price:finalPrice, qty:qty, size:size})
+                return
+                
+            }
+            return  
+        }
         await dispatch({type:"ADD",id:props.foodItem._id,name:props.foodItem.name, price:finalPrice, qty:qty, size:size})
-        console.log(data)
+        // await console.log(data)
     }
-    let finalPrice = qty * parseInt(options[size]);
+    console.log(props.foodItem);
+    let finalPrice = qty * parseInt(options[size], 10);
     useEffect(()=>{
         setSize(priceRef.current.value)
     },[])
     return (
+
         <div>
-            <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
+            
+            <div className="card mt-3" style={{ "width": "20rem", "maxHeight": "360px" }}>
                 <img src={props.foodItem.img}  class="card-img-top" alt="..." style={{height:"180px", width:"full", objectFit:"fill"}} />
                 <div className="card-body">
                     <h5 className="card-title">{props.foodItem.name}</h5>
-                    {/* <p className="card-text">This is some important text.</p> */}
                     <div className='container w-100'>
                         <select className='m-2 h-100 bg-danger rounded' onChange={(e)=> setQty(e.target.value)}>
-                            {Array.from(Array(6), (e, i) => {
+                            {Array.from(Array(8), (e, i) => {
                                 return (
                                     <option key={i + 1} value={i + 1}>{i + 1}</option>
                                 )
@@ -50,7 +74,3 @@ export default function Card(props) {
         </div>
     )
 }
-
-
-
-// https://media.istockphoto.com/id/184946701/photo/pizza.jpg?s=1024x1024&w=is&k=20&c=L8h3NUxinbmrBkSc7ETJHHxlEn6qYX4xzqIahI1Nh10=
