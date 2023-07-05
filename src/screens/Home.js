3863868b1c6c4b57b0c0c59e403dd9e56b35e50e
Bhavesh20Patel <101475153+Bhavesh20Patel@users.noreply.github.com>
2023-07-05 23:09:01 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -30,33 +30,33 @@ export default function Home() {
     <div>
       <div><Navbar /></div>
       <div >
-      <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{objectFit:"contain !important"}}>
-  <div className="carousel-inner" id="carausel">
-  <div className="carousel-caption" style={{zIndex:"10"}}>
-        <div className="d-flex justify-content-center">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
-            {/* <button className="btn btn-outline-success text-white bg-danger" type="submit">Search</button> */}
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{ objectFit: "contain !important" }}>
+          <div className="carousel-inner" id="carausel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                {/* <button className="btn btn-outline-success text-white bg-danger" type="submit">Search</button> */}
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img src="https://source.unsplash.com/random/728x300/?burger" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/728x300/?pizza" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+            <div className="carousel-item">
+              <img src="https://source.unsplash.com/random/728x300/?starter" className="d-block w-100" style={{ filter: "brightness(30%)" }} alt="..." />
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-      </div>
-    <div className="carousel-item active">
-      <img src="https://source.unsplash.com/random/728x300/?burger" className="d-block w-100" style={{filter: "brightness(30%)"}} alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://source.unsplash.com/random/728x300/?pizza" className="d-block w-100" style={{filter: "brightness(30%)"}} alt="..."/>
-    </div>
-    <div className="carousel-item">
-      <img src="https://source.unsplash.com/random/728x300/?starter" className="d-block w-100" style={{filter: "brightness(30%)"}} alt="..."/>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
       </div>
       <div className="container">
         {
@@ -74,9 +74,9 @@ export default function Home() {
                       return (
                         <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
                           <Card foodName={filterItems.name}
-                          foodItem = {filterItems}
-                          options = {filterItems.options[0]}
-                          ImgSrc={filterItems.img}
+                            foodItem={filterItems}
+                            options={filterItems.options[0]}
+                            ImgSrc={filterItems.img}
                           ></Card>
                         </div>
                       )
@@ -88,9 +88,8 @@ export default function Home() {
             })
             : ""
         }
-        
-        <div><Footer /></div>
 
+        <div><Footer /></div>
 
       </div>
     </div>
